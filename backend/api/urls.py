878309c81f99ffb4a4a6b418eea93a0dd96537a0
Navaneet J <navaneetj001@gmail.com>
2025-06-10 12:16:06 +0530

    Added Auth

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import FarmerViewSet, BuyerViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'farmer', FarmerViewSet, basename='farmer')
@@ -8,4 +9,5 @@ router.register(r'buyer', BuyerViewSet, basename='buyer')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('auth/token/', obtain_auth_token),
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'farmer',
     'buyer',
     'api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
