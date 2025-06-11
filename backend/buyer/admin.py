f764f313cd70861ae5e22b4a8edbce0eaacec514
Navaneet J <navaneetj001@gmail.com>
2025-06-11 08:30:23 +0530

    Changed everything...

--- backend/buyer/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Buyer, CartItem, Order
-
-admin.site.register(Buyer)
-admin.site.register(CartItem)
-admin.site.register(Order)
--- backend/farmer/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Farmer, Produce
-
-admin.site.register(Farmer)
-admin.site.register(Produce)
